567d1b389c0780559aa9a3afee96f3badbe46b2d
tiago <tiagomateus.fonte13@ua.pt>
2021-11-18 15:59:18 +0000

    keyboard commands (incompleto) e argparse para modo

--- ar_paint.py
+++ ar_paint.py
@@ -6,6 +6,19 @@ import argparse
 import json
 import numpy as np
 
+def paintMode():
+    parser = argparse.ArgumentParser(description="PARI AR Paint")
+    parser.add_argument('-usp',
+                        '--use_shake_prevention',
+                        help="Use Shake Detection.",
+                        action="store_true")
+    parser.add_argument('-video',
+                        '--draw_on_video',
+                        help="Use video stream as paint screen",
+                        action="store_true")
+    args = vars(parser.parse_args())
+    return args
+
 
 def JsonReader(json_file): # Read and format json file information
     f = open(json_file)
@@ -87,7 +100,40 @@ def main():
         key = cv2.waitKey(1)
         if key != -1:
             break
+        # <----------------------------------- Keyboard Commands ---------------------------------------->
+        color = (255, 0, 0) #default color
+        radius = 5          #default radius
+        command = cv2.waitKey(1)  #keyboard command
+        if command == 114:  # Press 'r' to paint red
 
+            color = (0, 0, 255)
+            del command
+
+        elif command == 103:  # Press 'g' to paint green
+
+            color = (0, 255, 0)
+            del command
+
+        elif command == 98:  # Press 'b' to paint blue
+
+            color = (255, 0, 0)
+            del command
+
+        elif command == 43:  # Press '+' to get bigger radius
+            radius += 1
+            del command
+
+        elif command == 45:  # Press '-' to get smaller radius
+            if radius > 1:
+                radius -= 1
+            del command
+
+        elif command == 119:  # Press 'w' to save sketch
+
+
+        elif command == 113:  # Press 'q' to quit
+            cv2.destroyAllWindows()
+            break
 
 
 if __name__ == "__main__":
