40572f16b1ba8f66cc24ff95131479d20845b01a
ghdrope <pedro.cozinheiro@ua.pt>
2021-11-21 15:42:08 +0000

    #1 Coloquei o color_segmenter inicial criado pela @beatrizcanhaborges numa página chamada rascunhos (caso queiramos ir checkar alguma coisa que lá estava). E o color_segmenter que estamos a implementar e que está funcional passa a estar na ramificação principal. Nota: Está funcional, estou a acabar os comentários.

--- color_segmenter.py
+++ color_segmenter.py
@@ -1,88 +1,151 @@
 #!/usr/bin/python3
-import argparse
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Authors: Beatriz Borges, Joel Baptista, José Cozinheiro e Tiago Fonte
+# Course: PSR
+# Class: Aula 7
+# Date: 17 Nov. 2021
+# Description: Avaliação 2 (PSR Augmented Reality Paint)
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Importing Packages
+import cv2
 import numpy as np
 import json
-from functools import partial
-import cv2
 from colorama import Back, Fore
 
+# <=================================================  Global Variables  ===============================================>
 
-def onTrackBars(_,window_name):
-    minB = cv2.getTrackbarPos('minB', window_name)
-    maxB = cv2.getTrackbarPos('maxB', window_name)
-    minG = cv2.getTrackbarPos('minG', window_name)
-    maxG = cv2.getTrackbarPos('maxG', window_name)
-    minR = cv2.getTrackbarPos('minR', window_name)
-    maxR = cv2.getTrackbarPos('maxR', window_name)
+global minimumb, maximumb, minimumg, maximumg, minimumr, maximumr
+
+minimumb = 0
+maximumb = 255
+minimumg = 0
+maximumg = 255
+minimumr = 0
+maximumr = 255
+
+def ontrackbarminb(minb):
+    global minimumb
+    minimumb = minb
 
 
-    limits = {'B': {'min': minB, 'max': maxB},
-              'G': {'min': minG, 'max': maxG},
-              'R': {'min': minR, 'max': maxR}}
+def ontrackbarmaxb(maxb):
+    global maximumb
+    maximumb = maxb
 
-    mins = np.array([limits['B']['min'], limits['G']['min'], limits['R']['min']])
-    maxs = np.array([limits['B']['max'], limits['G']['max'], limits['R']['max']])
 
+def ontrackbarming(ming):
+    global minimumg
+    minimumg = ming
+
+
+def ontrackbarmaxg(maxg):
+    global maximumg
+    maximumg = maxg
+
+
+def ontrackbarminr(minr):
+    global minimumr
+    minimumr = minr
+
+
+def ontrackbarmaxr(maxr):
+    global maximumr
+    maximumr = maxr
 
 
 def main():
 
-#---------------------INITIALIZATION-------------------------
+    # <================================================  INITIALIZATION  ==============================================>
 
     capture = cv2.VideoCapture(0)
-    # Window name definition
-    window_name_ori = 'Camera'
-    window_name_seg = 'Segmented image'
+    # window_name_original = 'Camera'
 
-
+    # Display Initial Relevant Info
     if capture.isOpened() is True:
-        print(Back.GREEN + 'Starting video' + Back.RESET)
-        print(Fore.LIGHTYELLOW_EX + 'Press w to exit and save' + Fore.RESET)
+        print('\n' + Back.GREEN + 'Starting video' + Back.RESET)
+        print('\n' + Fore.CYAN + 'Press w to exit and save color limits to file' + Fore.RESET)
         print(Fore.RED + 'Press q to exit without saving the threshold' + Fore.RESET)
 
-    # Show image
-    cv2.namedWindow(window_name_ori, cv2.WINDOW_NORMAL)
-    cv2.namedWindow(window_name_seg, cv2.WINDOW_NORMAL)
+    # Create Window (600 x 600) to display Normal Image
+    cv2.namedWindow('Camera', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Camera', 600, 600)
 
-    onTrackbar_partial = partial(onTrackBars, window_name=window_name_seg)
+    # Create Window to display Segmented Image
+    window_name = 'Segmented'
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
 
-    # Create all trackbar
-    cv2.createTrackbar('minB', window_name_seg, 0, 255, onTrackbar_partial)
-    cv2.createTrackbar('maxB', window_name_seg, 0, 255, onTrackbar_partial)
-    cv2.createTrackbar('minG', window_name_seg, 0, 255, onTrackbar_partial)
-    cv2.createTrackbar('maxG', window_name_seg, 0, 255, onTrackbar_partial)
-    cv2.createTrackbar('minR', window_name_seg, 0, 255, onTrackbar_partial)
-    cv2.createTrackbar('maxR', window_name_seg, 0, 255, onTrackbar_partial)
+    # <==================================================  TRACKBARS  ================================================>
 
+    # Implement a Set of Trackbars in Order to User be Able to Control Values of Binarization Threshold
+    cv2.createTrackbar('min B', window_name, 0, 255, ontrackbarminb)
+    cv2.createTrackbar('max B', window_name, 0, 255, ontrackbarmaxb)
+    cv2.createTrackbar('min G', window_name, 0, 255, ontrackbarming)
+    cv2.createTrackbar('max G', window_name, 0, 255, ontrackbarmaxg)
+    cv2.createTrackbar('min R', window_name, 0, 255, ontrackbarminr)
+    cv2.createTrackbar('max R', window_name, 0, 255, ontrackbarmaxr)
+
+    # Set Maximum Trackbars to a Default Beginning Position of 255
+    cv2.setTrackbarPos('max B', window_name, 255)
+    cv2.setTrackbarPos('max G', window_name, 255)
+    cv2.setTrackbarPos('max R', window_name, 255)
+
+    # Trackbars Init
+    ontrackbarminb(0)
+    ontrackbarmaxb(255)
+    ontrackbarming(0)
+    ontrackbarmaxg(255)
+    ontrackbarminr(0)
+    ontrackbarmaxr(255)
+
+    # <==========================================  COLOR SEGMENTATION RESULTS  ========================================>
+
+    #
     while capture.isOpened():
-        # Get an image from the camera and show
-        _, frame = capture.read()
-        cv2.imshow(window_name_ori, frame)
-        key = cv2.waitKey(1)  # Wait a key to stop the program
+        _, frame = capture.read()  #
+        cv2.imshow('Camera', frame)  # Display Normal Image
 
-        # ranges = {'limits': {'B': {'max': maxB, 'min': minB},
-        #                      'G': {'max': maxG, 'min': minG},
-        #                      'R': {'max': maxR, 'min': minR}}}
-        ranges = {'limits': {'B': {'max': 255, 'min': 0},
-                             'G': {'max': 255, 'min': 0},
-                             'R': {'max': 255, 'min': 229}}}
+        #
+        ranges = {'limits': {'B': {'min': minimumb, 'max': maximumb},
+                             'G': {'min': minimumg, 'max': maximumg},
+                             'R': {'min': minimumr, 'max': maximumr}}}
 
+        #
         mins = np.array([ranges['limits']['B']['min'], ranges['limits']['G']['min'], ranges['limits']['R']['min']])
         maxs = np.array([ranges['limits']['B']['max'], ranges['limits']['G']['max'], ranges['limits']['R']['max']])
 
+        #
+        mask = cv2.inRange(frame, mins, maxs)
 
-        cv2.namedWindow(window_name_seg)
+        #
+        cv2.imshow('Segmented', mask)
+        cv2.namedWindow('Segmented', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Segmented', 600, 600)
 
-        key = cv2.waitKey(1)
-        # Keyboard inputs to finish the cycle
+        # <======================================  SAVE / NOT SAVE PROGRESS  =========================================>
+
+        key = cv2.waitKey(1)  # Wait a Key to stop the Program
+
+        # Use "q" (Quit) Key to End the Program without saving the JSON File
         if key == ord('q'):
-            print(Fore.RED + 'You pressed "q" so you quit the program' + Fore.RESET)
+            print('\nProgram ending without saving progress')
             break
-    # ------------------TERMINATING-------------------------------------
+
+        # Use "w" (Write) Key to End the Program saving and writing the JSON File
+        elif key == ord('w'):
+            file_name = 'limits.json'
+            with open(file_name, 'w') as file_handle:
+                print('\nWriting color limits to file ' + file_name)
+                print(ranges)
+                json.dump(ranges, file_handle)
+            break
+
+    # <===========================================  TERMINATING  =========================================>
 
     capture.release()
     cv2.destroyAllWindows()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
